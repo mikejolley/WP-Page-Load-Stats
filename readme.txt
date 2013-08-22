18c1df9f5a595ba4340f6d82dbf851ad21c4bd7a
Mike Jolley <mike.jolley@me.com>
2013-08-22 22:29:20 +0100

    Version bump

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: mikejolley
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=mike.jolley@me.com&currency_code=&amount=&return=&item_name=Donation+for+WP+Page+Load+Stats
 Tags: query, counter, developer, debug, memory, footprint
 Requires at least: 3.0
-Tested up to: 3.4
-Stable tag: 1.0.0
+Tested up to: 3.6
+Stable tag: 1.0.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -29,6 +29,9 @@ If you'd like to contribute to the plugin you can find it on GitHub: [https://gi
 1. Stats shown in the footer
 
 == Changelog ==
+
+= 1.0.1 =
+* Markup fix
 
 = 1.0.0 =
 * Initial release!
--- wp-page-load-stats.php
+++ wp-page-load-stats.php
@@ -3,56 +3,56 @@
 Plugin Name: WP Page Load Stats
 Plugin URI: http://mikejolley.com/projects/wp-page-load-stats/
 Description: Display memory, page load time, average load time and query count in the footer. Requires PHP 5.2.0+
-Version: 1.0.0
+Version: 1.0.1
 Author: Mike Jolley
 Author URI: http://mikejolley.com
 */
 
 class WP_Page_Load_Stats {
-	
+
 	private $average_option;
-	
+
 	/**
 	 * Gets things started
 	 */
 	function __construct() {
 		// Init
 		add_action( 'init', array( &$this, 'init' ) );
-		
+
 		// Frontend
 		add_action( 'wp_head', array( &$this, 'wp_head' ) );
 		add_action( 'wp_footer', array( &$this, 'wp_footer' ) );
-		
+
 		// Backend
 		add_action( 'admin_head', array( &$this, 'wp_head' ) );
 		add_action( 'admin_footer', array( &$this, 'wp_footer' ) );
-		
+
 		// Enqueue
 		add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue' ) );
 		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue' ) );
-		
+
 		// Where to store averages
 		$this->average_option = is_admin() ? 'wp_pls_admin_load_times' : 'wp_pls_load_times';
 	}
-	
+
 	/**
 	 * init function.
-	 * 
+	 *
 	 * @access public
 	 */
 	function init() {
 		load_plugin_textdomain( 'wp_pls', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-		
+
 		if ( isset( $_GET['reset_wp_pls_stats'] ) && $_GET['reset_wp_pls_stats'] == 1 ) {
 			update_option( $this->average_option, array() );
 			wp_safe_redirect(  wp_get_referer() );
 			exit;
 		}
 	}
-	
+
 	/**
 	 * wp_head function.
-	 * 
+	 *
 	 * @access public
 	 */
 	function wp_head() {
@@ -63,28 +63,28 @@ class WP_Page_Load_Stats {
 			}
 		</script>";
 	}
-		
+
 	/**
 	 * wp_footer function.
-	 * 
+	 *
 	 * @access public
 	 */
 	function wp_footer() {
 		$this->display();
 	}
-	
+
 	/**
 	 * enqueue function.
-	 * 
+	 *
 	 * @access public
 	 */
 	function enqueue() {
         wp_enqueue_style( 'wp_pls-style', plugins_url('style.css', __FILE__) );
 	}
-	
+
 	/**
 	 * display function.
-	 * 
+	 *
 	 * @access public
 	 */
 	function display() {
@@ -95,16 +95,16 @@ class WP_Page_Load_Stats {
 		$memory_peak_usage 	= round( $this->convert_bytes_to_hr( memory_get_peak_usage() ), 2 );
 		$memory_limit 		= round( $this->convert_bytes_to_hr( $this->let_to_num( WP_MEMORY_LIMIT ) ), 2 );
 		$load_times			= array_filter( (array) get_option( $this->average_option ) );
-		
+
 		$load_times[]		= $timer_stop;
-		
+
 		// Update load times
 		update_option( $this->average_option, $load_times );
-		
+
 		// Get average
 		if ( sizeof( $load_times ) > 0 )
 			$average_load_time = round( array_sum( $load_times ) / sizeof( $load_times ), 4 );
-		
+
 		// Display the info
 		?>
 		<div id="wp_pls">
@@ -121,10 +121,10 @@ class WP_Page_Load_Stats {
 		</div>
 		<?php
 	}
-	
+
 	/**
 	 * let_to_num function.
-	 * 
+	 *
 	 * This function transforms the php.ini notation for numbers (like '2M') to an integer
 	 *
 	 * @access public
@@ -147,11 +147,11 @@ class WP_Page_Load_Stats {
 		        $ret *= 1024;
 	    }
 	    return $ret;
-	}	
-	
+	}
+
 	/**
 	 * convert_bytes_to_hr function.
-	 * 
+	 *
 	 * @access public
 	 * @param mixed $bytes
 	 */
@@ -162,7 +162,7 @@ class WP_Page_Load_Stats {
 		$size = pow(1024, $log - $power);
 		return $size . $units[$power];
 	}
-	
+
 }
 
 $WP_Page_Load_Stats = new WP_Page_Load_Stats();
