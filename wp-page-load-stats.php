02376f40ebe81a90475783ba58dfef2f00ebdf60
Mike Jolley <mike.jolley@me.com>
2015-11-26 16:11:50 +0000

    Code tidy

--- wp-page-load-stats.php
+++ wp-page-load-stats.php
@@ -3,62 +3,61 @@
 Plugin Name: WP Page Load Stats
 Plugin URI: http://mikejolley.com/projects/wp-page-load-stats/
 Description: Display memory, page load time, average load time and query count in the footer. Requires PHP 5.2.0+
-Version: 1.0.1
+Version: 1.0.2
+Requires at least: 3.0
+Tested up to: 4.4
 Author: Mike Jolley
 Author URI: http://mikejolley.com
+Text Domain: wp-page-load-stats
+Domain Path: /languages/
 */
 
+/**
+ * WP_Page_Load_Stats Class
+ */
 class WP_Page_Load_Stats {
 
+	/**
+	 * Stores the name of the option where averages get saved.
+	 * @var string
+	 */
 	private $average_option;
 
 	/**
 	 * Gets things started
 	 */
-	function __construct() {
-		// Init
-		add_action( 'init', array( &$this, 'init' ) );
-
-		// Frontend
-		add_action( 'wp_head', array( &$this, 'wp_head' ) );
-		add_action( 'wp_footer', array( &$this, 'wp_footer' ) );
-
-		// Backend
-		add_action( 'admin_head', array( &$this, 'wp_head' ) );
-		add_action( 'admin_footer', array( &$this, 'wp_footer' ) );
-
-		// Enqueue
-		add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue' ) );
-		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue' ) );
-
-		// Where to store averages
-		$this->average_option = is_admin() ? 'wp_pls_admin_load_times' : 'wp_pls_load_times';
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'wp_head', array( $this, 'wp_head' ) );
+		add_action( 'wp_footer', array( $this, 'wp_footer' ) );
+		add_action( 'admin_head', array( $this, 'wp_head' ) );
+		add_action( 'admin_footer', array( $this, 'wp_footer' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
 	}
 
 	/**
 	 * init function.
-	 *
-	 * @access public
 	 */
-	function init() {
-		load_plugin_textdomain( 'wp_pls', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	public function init() {
+		$this->average_option = is_admin() ? 'wp_pls_admin_load_times' : 'wp_pls_load_times';
+
+		load_plugin_textdomain( 'wp-page-load-stats', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 		if ( isset( $_GET['reset_wp_pls_stats'] ) && $_GET['reset_wp_pls_stats'] == 1 ) {
-			update_option( $this->average_option, array() );
-			wp_safe_redirect(  wp_get_referer() );
+			delete_option( $this->average_option );
+			wp_safe_redirect( wp_get_referer() );
 			exit;
 		}
 	}
 
 	/**
 	 * wp_head function.
-	 *
-	 * @access public
 	 */
-	function wp_head() {
+	public function wp_head() {
 		echo "<script type='text/javascript'>
 			function wp_pls_hide(){
-			   var wpplsDiv = document.getElementById('wp_pls');
+			   var wpplsDiv = document.getElementById('wp-page-load-stats');
 			   wpplsDiv.style.display = 'none';
 			}
 		</script>";
@@ -66,53 +65,47 @@ class WP_Page_Load_Stats {
 
 	/**
 	 * wp_footer function.
-	 *
-	 * @access public
 	 */
-	function wp_footer() {
+	public function wp_footer() {
 		$this->display();
 	}
 
 	/**
 	 * enqueue function.
-	 *
-	 * @access public
 	 */
-	function enqueue() {
+	public function enqueue() {
         wp_enqueue_style( 'wp_pls-style', plugins_url('style.css', __FILE__) );
 	}
 
 	/**
 	 * display function.
-	 *
-	 * @access public
 	 */
-	function display() {
+	public function display() {
 		// Get values we're displaying
 		$timer_stop 		= timer_stop(0);
 		$query_count 		= get_num_queries();
-		$memory_usage 		= round( $this->convert_bytes_to_hr( memory_get_usage() ), 2 );
-		$memory_peak_usage 	= round( $this->convert_bytes_to_hr( memory_get_peak_usage() ), 2 );
-		$memory_limit 		= round( $this->convert_bytes_to_hr( $this->let_to_num( WP_MEMORY_LIMIT ) ), 2 );
-		$load_times			= array_filter( (array) get_option( $this->average_option ) );
-
+		$memory_usage 		= round( size_format( memory_get_usage() ), 2 );
+		$memory_peak_usage 	= round( size_format( memory_get_peak_usage() ), 2 );
+		$memory_limit 		= round( size_format( $this->let_to_num( WP_MEMORY_LIMIT ) ), 2 );
+		$load_times			= array_filter( (array) get_option( $this->average_option, array() ) );
 		$load_times[]		= $timer_stop;
 
 		// Update load times
 		update_option( $this->average_option, $load_times );
 
 		// Get average
-		if ( sizeof( $load_times ) > 0 )
+		if ( sizeof( $load_times ) > 0 ) {
 			$average_load_time = round( array_sum( $load_times ) / sizeof( $load_times ), 4 );
+		}
 
 		// Display the info
 		?>
 		<div id="wp_pls">
 			<ul>
-				<li><?php printf( __( '%s queries in %s seconds.', 'wp_pls' ), $query_count, $timer_stop ); ?></li>
-				<li><?php printf( __( 'Average load time of %s (%s runs).', 'wp_pls' ), $average_load_time, sizeof( $load_times ) ); ?></li>
-				<li><?php printf( __( '%s out of %s MB (%s) memory used.', 'wp_pls' ), $memory_usage, $memory_limit, round( ( $memory_usage / $memory_limit ), 2 ) * 100 . '%' ); ?></li>
-				<li><?php printf( __( 'Peak memory usage %s MB.', 'wp_pls' ), $memory_peak_usage ); ?></li>
+				<li><?php printf( __( '%s queries in %s seconds.', 'wp-page-load-stats' ), $query_count, $timer_stop ); ?></li>
+				<li><?php printf( __( 'Average load time of %s (%s runs).', 'wp-page-load-stats' ), $average_load_time, sizeof( $load_times ) ); ?></li>
+				<li><?php printf( __( '%s out of %s MB (%s) memory used.', 'wp-page-load-stats' ), $memory_usage, $memory_limit, round( ( $memory_usage / $memory_limit ), 2 ) * 100 . '%' ); ?></li>
+				<li><?php printf( __( 'Peak memory usage %s MB.', 'wp-page-load-stats' ), $memory_peak_usage ); ?></li>
 			</ul>
 			<div class="actions">
 				<a onclick="wp_pls_hide()" href="javascript:void(0);">&times;</a>
@@ -127,13 +120,12 @@ class WP_Page_Load_Stats {
 	 *
 	 * This function transforms the php.ini notation for numbers (like '2M') to an integer
 	 *
-	 * @access public
 	 * @param $size
 	 * @return int
 	 */
-	function let_to_num( $size ) {
-	    $l 		= substr( $size, -1 );
-	    $ret 	= substr( $size, 0, -1 );
+	public function let_to_num( $size ) {
+	    $l 		 = substr( $size, -1 );
+	    $ret 	 = substr( $size, 0, -1 );
 	    switch( strtoupper( $l ) ) {
 		    case 'P':
 		        $ret *= 1024;
@@ -148,21 +140,6 @@ class WP_Page_Load_Stats {
 	    }
 	    return $ret;
 	}
-
-	/**
-	 * convert_bytes_to_hr function.
-	 *
-	 * @access public
-	 * @param mixed $bytes
-	 */
-	function convert_bytes_to_hr( $bytes ) {
-		$units = array( 0 => 'B', 1 => 'kB', 2 => 'MB', 3 => 'GB' );
-		$log = log( $bytes, 1024 );
-		$power = (int) $log;
-		$size = pow(1024, $log - $power);
-		return $size . $units[$power];
-	}
-
 }
 
 $WP_Page_Load_Stats = new WP_Page_Load_Stats();
